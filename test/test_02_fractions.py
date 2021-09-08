9c68c04af55c98a2259ed888d45f1666cbd19efe
amathael <dan.io.oreshnikov@gmail.com>
2021-09-08 05:16:46 +0300

    Tasks 01, 02 and Test for 02 are added

---
+++ test/test_02_fractions.py
@@ -0,0 +1,28 @@
+from tasks.t02_fractions import add_fractions
+from random import randint
+
+
+def check_baseline():
+    for data in [
+        ((1, 1, 1, 1), (2, 1)),
+        ((-1, 1, 1, -1), (-2, 1)),
+        ((2, 3, 1, -6), (1, 2)),
+    ]:
+        assert add_fractions(*data[0]) == data[1]
+
+
+def check_random(n_iter=10, eps=1e-9):
+    def __gcd(x, y):
+        return x if y == 0 else __gcd(y, x % y)
+
+    for it in range(n_iter):
+        a, b, c, d = [randint(-1000, 1000) for _ in range(4)]
+        while b == 0:
+            b = randint(-1000, 1000)
+        while d == 0:
+            d = randint(-1000, 1000)
+        x, y = add_fractions(a, b, c, d)
+
+        assert abs(a / b + c / d - x / y) <= eps  # check near
+        assert __gcd(x, y) == 1  # irreducible
+        assert y > 0  # denominator positive
